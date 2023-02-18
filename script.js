d7a9958b3f562093dcfa5b853b1f912f03991aa5
danilaverbytskyy <dod.shreder@gmail.com>
2023-02-18 17:37:38 +0300

    Первая полностью работающая версия сайта

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 function CalculatesHowManyDaysYouLive() {
-	var day,month,year;
-	var text;
-	day=document.getElementById("day").value;
-	month=document.getElementById("month").value;
-	year=document.getElementById("year").value;
+	let day, month, year;
+	let text;
+	day = document.getElementById("day").value;
+	month = document.getElementById("month").value;
+	year = document.getElementById("year").value;
 
-	if(isNaN(day) || isNaN(month) || isNaN(year) ||
-	 day<1 || month<1 || year<1 || day>31 || month>12){
-		text="Введенные данные не корректны";
+	if (isNaN(day) || isNaN(month) || isNaN(year)) {
+		text = "Введенные данные не корректны";
 	}
-	else{
-		var today=new Date();
-		var birthday=new Date(year,month,day);
-		text=String((today-birthday+1)/86400);
-		text="Вы прожили "+text+" дней";
+	else {
+		if (day < 1 || month < 1 || year < 1 || day > 31 || month >= 13 || year <= 0 || String(day).length > 2 || String(month).length > 2)
+			text = "Введенные данные не корректны";
+		else {
+			let today = new Date();
+			let birthday = new Date(year, month - 1, day, 0, 0, 0, 0);
+			/*Месяц должен быть между 0 и 11 в Data конструкторе*/
+			text = (today - birthday) / (1000 * 3600 * 24);
+			text = Math.floor(text);
+			text = "Вы прожили " + String(text) + " дней";
+		}
 	}
 	document.getElementById("answer").innerHTML = text;
 }
